1e35129f04c53b846df70ee77dee0fd64b969541
Jasmin Kling <jasmin.kling@exxcellent.de>
2020-05-21 12:34:15 +0200

    Updates

---
+++ precache-manifest.63d873271e6413cf76922ef2b3e2c64b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9e49234a44764c2c8a0d6892cd132f7",
+    "url": "/reaction-workout/index.html"
+  },
+  {
+    "revision": "b497108292fe53f3d467",
+    "url": "/reaction-workout/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "dff54e4206628d9c11cd",
+    "url": "/reaction-workout/static/css/main.c818fd79.chunk.css"
+  },
+  {
+    "revision": "b497108292fe53f3d467",
+    "url": "/reaction-workout/static/js/2.7bc07add.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/reaction-workout/static/js/2.7bc07add.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dff54e4206628d9c11cd",
+    "url": "/reaction-workout/static/js/main.502f2150.chunk.js"
+  },
+  {
+    "revision": "ccf560ccd49c4124364b",
+    "url": "/reaction-workout/static/js/runtime-main.d5e6d84a.js"
+  }
+]);
